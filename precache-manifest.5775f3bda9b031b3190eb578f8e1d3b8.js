48da9c2de0865f8ed903c75cdb8115bdfee74a92
duwei <1069646435@qq.com>
2021-04-10 16:12:25 +0800

    deploy

---
+++ precache-manifest.5775f3bda9b031b3190eb578f8e1d3b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12f544a7f72f5cd09518",
+    "url": "/QingXiu-J-website/css/app.6718cb55.css"
+  },
+  {
+    "revision": "0ad4f33e5a8b2629dfd6b6d0b614c001",
+    "url": "/QingXiu-J-website/index.html"
+  },
+  {
+    "revision": "12f544a7f72f5cd09518",
+    "url": "/QingXiu-J-website/js/app.2edc128e.js"
+  },
+  {
+    "revision": "dc3b8ac2ef20574ed499",
+    "url": "/QingXiu-J-website/js/chunk-vendors.62fab2bd.js"
+  },
+  {
+    "revision": "42bfa4ba9edfb3426e53b3524294dc7f",
+    "url": "/QingXiu-J-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/QingXiu-J-website/robots.txt"
+  }
+]);
